63d36dce25e39072727e57d95b021338485a045a
Jens Laufer <jenslaufer@gmail.com>
2016-01-26 20:37:27 +0100

    deploeyed

--- js/main.js
+++ js/main.js
@@ -1,44 +1,31 @@
-$(document).ready(function() {
-
-    // Initialize Parse with your Parse application & javascript keys
-    Parse.initialize("fe50rfKZvPJARdcqFrDRRs77qaudvQGPU5JyMd0L", "Er5TBVwLJAOuSOaL6kjslLZTgVD6pSzNqWGGcEAe");
-
-    // Setup the form to watch for the submit event
-    $('#umzug_erfassen').submit(function(e) {
-        e.preventDefault();
-
-        // Grab the elements from the form to make up
-        // an object containing name, email and message
-        var data = {
-            site: "pickito.de",
-            subject: "Neuer Umzug",
-            sender: "Jens Laufer <jens.laufer@pickito.de>",
-            Empfaenger: document.getElementById('email').value,
-            Datum: document.getElementById('date').value,
-            Groesse: document.getElementById('size').value,
-            AnzahlPersonen: document.getElementById('numberOfPersons').value,
-            Von: {
-                Strasse: document.getElementById('fromStreet').value,
-                Plz: document.getElementById('fromZip').value,
-                Stadt: document.getElementById('fromCity').value
-            },
-            Nach: {
-                Strasse: document.getElementById('toStreet').value,
-                Plz: document.getElementById('toZip').value,
-                Stadt: document.getElementById('toCity').value
+angular.module('app', ['ui.bootstrap']).controller('handleController', ['$scope',
+    function($scope) {
+        Parse.initialize("fe50rfKZvPJARdcqFrDRRs77qaudvQGPU5JyMd0L", "Er5TBVwLJAOuSOaL6kjslLZTgVD6pSzNqWGGcEAe");
+        $scope.Umzug = {
+            Dienstleistungen: {
+                Einpacken: "NEIN",
+                Abbau: "NEIN",
+                Transport: "JA",
+                Aufbau: "NEIN",
+                Auspacken: "NEIN"
             }
+        };
+
+        $scope.create = function(Umzug) {
+            Umzug.site = "pickito.de";
+            Umzug.subject = "Neuer Umzug";
+            Umzug.sender = "Jens Laufer <jens.laufer@pickito.de>";
+
+            Parse.Cloud.run("handle", Umzug, {
+                success: function(object) {
+                    $('#response').html('Email sent!').addClass('success').fadeIn('fast');
+                },
+
+                error: function(object, error) {
+                    console.log(error);
+                    $('#response').html('Error! Email not sent!').addClass('error').fadeIn('fast');
+                }
+            });
         }
-
-        Parse.Cloud.run("handle", data, {
-            success: function(object) {
-                $('#response').html('Email sent!').addClass('success').fadeIn('fast');
-            },
-
-            error: function(object, error) {
-                console.log(error);
-                $('#response').html('Error! Email not sent!').addClass('error').fadeIn('fast');
-            }
-        });
-    });
-
-});
+    }
+]);
